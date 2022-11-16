c0be6931ad664920c8a83c44d73284abacf77e77
Bishop b <iyiolablessing98@gmail.com>
2022-11-16 05:15:03 +0100

    done

---
+++ java.js
@@ -0,0 +1,25 @@
+let firstname = "BLESSING";
+let lastname = "IYIOLA";
+let middlename = "OLALEKAN";
+let othername = "IFEOLUWA";
+
+if (firstname == "BLESSING"){
+    document.getElementById("fn").innerHTML= firstname;
+    
+}
+
+else if(firstname == "IYIOLA"){
+    
+}
+ if (middlename == "OLALEKAN"){
+    console.log(middlename);
+    document.getElementById('mn').innerHTML= middlename;
+}
+if (lastname == "IYIOLA"){
+    console.log("bbbb")
+    document.getElementById('ln').innerHTML= lastname;
+}
+if  (othername == "IFEOLUWA"){
+    console.log('gggg')
+    document.getElementById('on').innerHTML= lastname;
+}
